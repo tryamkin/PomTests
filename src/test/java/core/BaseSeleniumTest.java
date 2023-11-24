e6a3dbb0b4a8ff58811c1911b6e8bf6ead66c228
tryam <tryamkin@gmail.com>
2023-11-24 22:20:37 +0200

    game with headless v3

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -4,7 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
@@ -13,18 +15,18 @@ import java.util.concurrent.TimeUnit;
 abstract public class BaseSeleniumTest {
     protected WebDriver driver;
 
-    @BeforeSuite
+    @BeforeClass
     public void setUp(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-      // driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+      //  driver = new ChromeDriver();
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         BaseSeleniumPage.setDriver(driver);
     }
 
-    @AfterSuite
+    @AfterClass
     public void tearDown(){
         driver.close();
         driver.quit();
