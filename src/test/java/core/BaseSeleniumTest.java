7f191612304092dec5b8f8b6ad674a5cf983992d
tryam <tryamkin@gmail.com>
2023-11-27 20:16:17 +0200

    Allure add screenshot whit method name

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -8,13 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
-
 import java.io.ByteArrayInputStream;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 abstract public class BaseSeleniumTest {
     protected WebDriver driver;
@@ -42,10 +38,9 @@ abstract public class BaseSeleniumTest {
     }
 
     public void takeScreenShot(){
-        Allure.addAttachment(new Object() {}
-                .getClass()
-                .getEnclosingMethod()
-                .getName(), new ByteArrayInputStream(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES)));
+        Allure.addAttachment(new Throwable()
+                .getStackTrace()[1]
+                .getMethodName(), new ByteArrayInputStream(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES)));
     }
 
     @AfterClass
--- src/test/java/exampleForYourSite/GooglePageTest.java
+++ src/test/java/exampleForYourSite/GooglePageTest.java
@@ -1,18 +1,22 @@
 package exampleForYourSite;
 
 import core.BaseSeleniumTest;
+import io.qameta.allure.*;
 import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.CustomAttribute;
-
 import org.testng.annotations.Test;
 
 
 public class GooglePageTest extends BaseSeleniumTest {
 
     @Test(priority = 1)
-    @Description("First test")
+    @Description("First test from Description")
+    @Owner("Owner Name")
+    @Severity(SeverityLevel.CRITICAL)
     @CustomAttribute(name = "Taras ")
+    @Step ("First step ")
+    @Issue(value = "R2D2 ")
     public void firstTest (){
         MainPage mainPage = new MainPage();
         mainPage.openMainPage();
@@ -25,7 +29,7 @@ public class GooglePageTest extends BaseSeleniumTest {
         MainPage mainPage = new MainPage();
         mainPage.searchSomething("Selenium");
         Assert.assertTrue(mainPage.getUrlPage().contains("search"));
-        takeScreenShot();
+
     }
 
 
