ad78e90dedbc1a445b1283eeac66820905954fb0
tryam <tryamkin@gmail.com>
2023-11-27 16:13:58 +0200

    add Allure report

--- src/test/java/exampleForYourSite/MainPage.java
+++ src/test/java/exampleForYourSite/MainPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import java.util.logging.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MainPage extends BaseSeleniumPage {
@@ -15,11 +17,14 @@ public class MainPage extends BaseSeleniumPage {
 
     //Конструктор на главной странице
     public MainPage() {
-        driver.get("https://google.com.ua");
+
+        Logger log = Logger.getLogger(MainPage.class.getName());
+        log.info("open");
         PageFactory.initElements(driver, this);
     }
 
     public void openMainPage() {
+        driver.get("https://google.com.ua");
     }
 
     public String  getTitlePage() {
@@ -27,12 +32,14 @@ public class MainPage extends BaseSeleniumPage {
     }
 
     public void searchSomething(String search) {
+        openMainPage();
         searchField.click();
         searchField.sendKeys(search);
         searchField.sendKeys(Keys.ENTER);
     }
 
     public String getUrlPage() {
-     return    driver.getCurrentUrl();
+
+        return    driver.getCurrentUrl();
     }
 }
