22a64495351ce8d0c16ff507306554a9225275cd
tryam <tryamkin@gmail.com>
2023-12-10 20:00:44 +0200

    add login with coockies v2

--- src/test/java/coockieEnter/LoginWithCoockies.java
+++ src/test/java/coockieEnter/LoginWithCoockies.java
@@ -1,10 +1,8 @@
 package coockieEnter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.ByteArrayInputStream;
 import java.time.Duration;
 import java.util.Date;
 
@@ -20,35 +19,38 @@ import static io.restassured.RestAssured.given;
 public class LoginWithCoockies {
     protected static WebDriver driver;
 
-    public void setUpdrv(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox",
-                "--disable-gpu","--disable-dev-shm-usage",
-                "--window-size=1920,1080","--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+    public void takeScreenShot(){
+        Allure.addAttachment(new Throwable()
+                .getStackTrace()[1]
+                .getMethodName(), new ByteArrayInputStream(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES)));
     }
 
+    public void setUpdrv() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox",
+                "--disable-gpu", "--disable-dev-shm-usage",
+                "--window-size=1920,1080", "--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
 
 
     @Test
     public void openPage() {
         setUpdrv();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    driver.get("https://www.demoblaze.com/");
-    Cookie getCoockie = new Cookie("tokenp_","VXNlcjEyMzEyMzE3MDI4MjM=");
-    driver.manage().addCookie(getCoockie);
-    driver.navigate().refresh();
-
-   Cookie cookieUser = driver.manage().getCookieNamed("user");
-    driver.navigate().refresh();
-        System.out.println(cookieUser);
-
         driver.get("https://www.demoblaze.com/");
-
-
-      WebElement nameOfLoginnedUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"nameofuser\"]")));
+        takeScreenShot();
+        Cookie getCoockie = new Cookie("tokenp_", "VXNlcjEyMzEyMzE3MDI4MjM=");
+        driver.manage().addCookie(getCoockie);
+        driver.navigate().refresh();
+        Cookie cookieUser = driver.manage().getCookieNamed("user");
+        driver.navigate().refresh();
+        System.out.println(cookieUser);
+        driver.get("https://www.demoblaze.com/");
+        WebElement nameOfLoginnedUser = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"nameofuser\"]")));
         Assert.assertTrue(nameOfLoginnedUser.isDisplayed());
+        takeScreenShot();
         driver.close();
         driver.quit();
 
@@ -63,6 +65,6 @@ public class LoginWithCoockies {
             .then().log().all().extract().cookie("user");
 
         System.out.println(sessionid);*/
-}
+    }
 
 }
