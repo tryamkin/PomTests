fd5c1a268d313b479934b302bbcb0b98dbb4e81e
Taras Shevchenko <tryamkin@gmail.com>
2024-02-12 11:11:16 +0100

    Update TestValues.java

--- src/test/java/utils/TestValues.java
+++ src/test/java/utils/TestValues.java
@@ -2,5 +2,6 @@ package utils;
 
 public class TestValues {
     public static String SEARCH_EXAMPLE = "Selenium";
+        public static String SEARCH_EXAMPLE1 = "Selenium";
     public static String URL = "https://google.com.ua";
 }
